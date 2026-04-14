024fe2b03a42a0f864f6c5149995d089ce4a3229
Adrian Sin <adriansin55@gmail.com>
2026-04-14 04:09:06 -0400

    smart fan

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# smartFan
+Set Up
+Download the two libraries needed to run the code for this project.
+
+Temperature Sensor (DHT11): https://github.com/dhrubasaha08/DHT11
+
+LCD Screen (Liquid Crystal I2C): https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+
+Locate this project's 'Libraries' folder in your local files and insert the files you downloaded.
+
+Open the Arduino IDE.
+
+Connect your computer with the Arduino, verify the code, and upload it.
+
+Smart Fan System
+The Smart Fan System works autonomously for the most part. On start-up, the preferred temperature is set to 76 degrees Fahrenheit.
+
+If the DHT11 sensor detects the temperature dropping below the threshold, the Arduino will turn on the red LED and display that the 'heater is on' on the screen. To test this feature, the user can wait for the fan to cool down the temperature around the sensor.
+
+Otherwise, if the sensor detects the temperature rising above the threshold, the Arduino will turn on the blue LED and display that the 'fan is on' on the screen. A fan will also turn on to cool the air around the sensor. To test this feature, the user can wrap their hand around the sensor to heat it.
+
+If the user would like to adjust the preferred temperature, they can with the two buttons located on the breadboard.
+
+The breadboard is split into two sides, one side where the LEDs are located and the buttons on the other. With the button side facing towards the user, the left button would allow the user to lower the set temperature, and the right button would allow the user to raise the set temperature.
---
+++ code.ino
@@ -0,0 +1,109 @@
+#include <DHT11.h>             
+#include <LiquidCrystal_I2C.h>    
+
+LiquidCrystal_I2C lcd(0x27, 16, 3); 
+
+#define tempsensor 2  
+
+#define FAN 7
+#define RED_LIGHT 6
+#define BLUE_LIGHT 5
+#define Increase 13
+#define Decrease 12
+
+DHT11 dht(tempsensor);  
+
+int threshold = 76; 
+int pressed = 0;
+
+void setup() {
+  lcd.begin(); 
+
+  pinMode(FAN, OUTPUT); 
+  pinMode(Increase, INPUT_PULLUP);
+  pinMode(Decrease, INPUT_PULLUP);
+
+  pinMode(RED_LIGHT, OUTPUT);
+  pinMode(BLUE_LIGHT, OUTPUT);
+  digitalWrite(FAN, LOW);
+
+  Serial.begin(9600);  
+}
+
+void loop() {
+  Serial.println(threshold); 
+
+  int increaseButton = digitalRead(Increase);
+  int decreaseButton = digitalRead(Decrease);
+
+  delay(10);
+
+
+  if (decreaseButton == pressed) {
+    threshold -= 1;
+    lcd.clear();
+    lcd.setCursor(0, 0); 
+    lcd.print("Set Temperature");
+    lcd.setCursor(0, 1);  
+    lcd.print(threshold);
+    lcd.print(" F");
+    delay(1500);
+    lcd.clear();
+  }
+
+  if (increaseButton == pressed) {
+    threshold += 1;
+    lcd.clear();
+    lcd.setCursor(0, 0); 
+    lcd.print("Set Temperature");
+    lcd.setCursor(0, 1);  
+    lcd.print(threshold);
+    lcd.print(" F");
+    delay(1500);
+    lcd.clear();
+  }
+
+
+  float temperatureCelsius = dht.readTemperature();  
+  float temperatureFahrenheit = (temperatureCelsius * 9.0 / 5.0) + 32.0; 
+  int temperature = (int)temperatureFahrenheit;  
+
+  float humidity = dht.readHumidity();  
+
+  Serial.print("Temperature = ");
+  Serial.print(temperature);
+  Serial.println("°F");
+  Serial.print("Humidity = ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+
+  if (temperature > threshold) {
+    lcd.clear();
+    lcd.setCursor(0, 0);  
+    lcd.print("Temp: ");
+    lcd.print(temperature);
+    lcd.print(" F");
+
+    digitalWrite(FAN, HIGH);
+    digitalWrite(RED_LIGHT, LOW);
+    digitalWrite(BLUE_LIGHT, HIGH);
+    Serial.println("Fan ON");
+    lcd.setCursor(0, 1);  
+    lcd.print("Fan On");
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);  
+    lcd.print("Temp: ");
+    lcd.print(temperature);
+    lcd.print(" F");
+
+    digitalWrite(FAN, LOW);
+    digitalWrite(RED_LIGHT, HIGH);
+    digitalWrite(BLUE_LIGHT, LOW);
+    Serial.println("Fan OFF");
+    lcd.setCursor(0, 1);  
+    lcd.print("Heater On");
+  }
+
+}
